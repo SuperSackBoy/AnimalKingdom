ad112dd58ec99f523addc64aaee27d6837236c1f
Luca <945072@pdsb.net>
2023-05-20 22:57:10 -0400

    Added black bear

--- CardPanel.java
+++ CardPanel.java
@@ -10,12 +10,9 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
