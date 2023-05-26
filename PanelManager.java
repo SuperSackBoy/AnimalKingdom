f5c38272cca4ed7482ab8bb29b085a356c39200b
Aaron Kimbel <aaronkimbel2@gmail.com>
2023-05-26 09:37:55 -0400

    fixed surrender button using button

--- PanelManager.java
+++ PanelManager.java
@@ -121,10 +121,10 @@ public class PanelManager {
         frame.add(button);
 
         JButton surrenderButton = new JButton("Surrender");
-        button.setBounds(0,225,150,65);
-        button.addActionListener(e -> endTurn());
-        button.setFocusable(false);
-        frame.add(button);
+        surrenderButton.setBounds(0,225,150,65);
+        surrenderButton.addActionListener(e -> endTurn());
+        surrenderButton.setFocusable(false);
+        frame.add(surrenderButton);
 
         VPDisplay.setBounds(0,300,200,80);
         VPDisplay.setForeground(Color.white);
