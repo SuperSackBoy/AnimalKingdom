34417abdeeaa7328ab95149cbd11a09f38288c29
945072 <945072@pdsb.net>
2023-05-29 10:32:18 -0400

    Debug Codes added

--- debugCodes.java
+++ debugCodes.java
@@ -1,12 +1,40 @@
 public class debugCodes {
+    public static boolean allJone, mrJone;
     public static void useCodes(String code) {
-        System.out.println(mainMenu.debugCode);
 
         if (code.equalsIgnoreCase("Mr. Jone")){
-            if (mainMenu.debugCode.equalsIgnoreCase("Mr. Jone")) {
-                CardDeck.deckReset();
-                CardDeck.cardList.add(new Card("Mr. Jone", "imageAssets/cardSprites/MrJoneCard.png", 99, 99, 1));
-            }
+            CardDeck.deckReset();
+            mrJone = true;
+            System.out.println(mainMenu.debugCode);
+        }
+        if (code.equalsIgnoreCase("Oops all Jone!")) {
+            CardDeck.deckReset();
+            CardDeck.cardList.add(new Card("Mr. Jone", "imageAssets/cardSprites/MrJoneCard.png", 99, 99, 1));
+            allJone = true;
+            System.out.println(mainMenu.debugCode);
+        }
+
+        String codeNum = code;
+        codeNum = codeNum.replaceAll("[^\\d.]", "");
+        int codeNumInt;
+        try {
+            codeNumInt = Integer.parseInt(codeNum);
+        } catch (NumberFormatException e) {
+            codeNumInt = 0;
+        }
+
+        if (code.equalsIgnoreCase("Player HP " + codeNum)) {
+            Player.setHP(codeNumInt);
+            System.out.println(mainMenu.debugCode);
+        }
+        if (code.equalsIgnoreCase("AI HP "+ codeNum)) {
+            PanelManager.ai.HP = codeNumInt;
+            System.out.println(mainMenu.debugCode);
+        }
+
+        if (code.equalsIgnoreCase("Player VP " + codeNum)) {
+            Player.setMaxVP(codeNumInt);
+            System.out.println(mainMenu.debugCode);
         }
     }
 }
